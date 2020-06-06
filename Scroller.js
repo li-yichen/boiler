a5b98ab985c9de430b6c0ce6a442f38fa6848c5b
5ft3squirrel <38572217+li-yichen@users.noreply.github.com>
2020-06-05 21:15:38 -0700

    Update Scroller.js

--- Scroller.js
+++ Scroller.js
@@ -1,11 +1,9 @@
 // adapted from https://stackoverflow.com/questions/27496465/how-can-i-play-sound-in-a-chrome-extension#27496510
-
+// audio credits to WrathfulBeatz, 2kw Diesel Generator Sound *1 Hour* [Whitenoise / Background], https://www.youtube.com/watch?v=t8UYI1QqjxQ
 let url = chrome.runtime.getURL('generator.mp3');
 let audioElement = new Audio(url);
 
-// while (True) {
-	window.addEventListener('scroll', function() {
-		audioElement.volume = 1;
-		audioElement.play();
-	});
-// }
+window.addEventListener('scroll', function() {
+	audioElement.volume = 1;
+	audioElement.play();
+});
